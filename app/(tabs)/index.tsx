382be5475ecda96f67ccc37b045a1208979324be
AdamBurysek <adam.burysek@seznam.cz>
2023-11-26 11:06:15 +0100

    starting index page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,31 @@
-import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <Text style={styles.title}>Last Cigarette at 12:34</Text>
+      <Text style={styles.title}>45min. ago</Text>
+      <Text style={styles.title}>Today</Text>
+      <View style={styles.counterBox}>
+        <Text style={styles.cigaretteCount}>5</Text>
+        <Text style={styles.cigaretteCountText}>Cigarettes</Text>
+      </View>
+      <View>
+        <View>
+          <Text>Yesterday</Text>
+          <Text>21</Text>
+          <Text>every ~32min.</Text>
+        </View>
+        <View>
+          <Text>Left for today</Text>
+          <Text>10</Text>
+          <Text>every ~42min.</Text>
+        </View>
+      </View>
+      <TouchableOpacity>
+        <Text>PLUS</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,10 +35,34 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
-    backgroundColor: "white",
+    backgroundColor: "red",
+    borderColor: "red",
+    borderStyle: "solid",
+    marginTop: 100,
   },
   title: {
     fontSize: 20,
-    fontWeight: "bold",
+    fontFamily: "Roboto-Bold",
+  },
+  counterBox: {
+    backgroundColor: "green",
+    padding: 130,
+    borderRadius: 300,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cigaretteCount: {
+    position: "absolute",
+
+    fontSize: 144,
+    fontFamily: "Roboto-Medium",
+    top: 25,
+  },
+  cigaretteCountText: {
+    position: "absolute",
+    fontSize: 24,
+    fontFamily: "Roboto-Medium",
+    top: 190,
   },
 });
